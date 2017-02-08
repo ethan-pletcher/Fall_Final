57b90db13a0ec43821ef2da13caaabb5db8abd21
Ethan S Pletcher <ep64523@srcs.loc>
2017-02-08 08:50:45 -0800

    Fixed first 2 tests

--- calculator_tests.js
+++ calculator_tests.js
@@ -1,13 +1,15 @@
 // Test for inputing digits
 QUnit.test("Add digits test", function (assert) {
-    addDigit('1');
-    addDigit('2');
-    assert.equal(document.getElementById("screen").value, "12", "Passed - Expected 12");
+          addDigit('1');
+          addDigit('2');
+          assert.equal(document.getElementById("screen").value, "12", "Passed - Expected 12");
+          allClear();
 });
+
 // Test adding one, then two decimals
 QUnit.test("Add decimal test", function (assert) {
-    addDecimal();
-    addDigit('2');
-    addDecimal();
-    assert.equal(document.getElementById("screen").value, "0.2", "Passed - Expected 0.2");
+          addDecimal();
+          addDigit('2');
+          addDecimal();
+          assert.equal(document.getElementById("screen").value, ".2", "Passed - Expected .2");
 });
