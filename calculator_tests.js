72fddfd2687d2d045f7136023dbc326dd0054b1f
MatthewGess <torresvilla79@gmail.com>
2017-02-08 08:57:52 -0800

    Fixed conflicts

--- calculator_tests.js
+++ calculator_tests.js
@@ -1,25 +1,15 @@
 // Test for inputing digits
 QUnit.test("Add digits test", function (assert) {
-<<<<<<< HEAD
     addDigit('1');
     addDigit('2');
     assert.equal(document.getElementById("screen").value, "12", "Passed - Expected 12");
-    currentInputdisp = '0'
-=======
-          addDigit('1');
-          addDigit('2');
-          assert.equal(document.getElementById("screen").value, "12", "Passed - Expected 12");
-          allClear();
->>>>>>> origin/master
+    allClear();
 });
-
 // Test adding one, then two decimals
 QUnit.test("Add decimal test", function (assert) {
-<<<<<<< HEAD
     addDecimal();
     addDigit('2');
-    addDecimal();
-    assert.equal(document.getElementById("screen").value, "0.2", "Passed - Expected 0.2");
+    assert.equal(document.getElementById("screen").value, ".2", "Passed - Expected .2");
     currentInputdisp = '0'
 });
 QUnit.test("Test the displayErrorMessage function.", function (assert) {
@@ -27,10 +17,4 @@ QUnit.test("Test the displayErrorMessage function.", function (assert) {
     displayErrorMessage()
     assert.equal(document.getElementById("screen2").value, "FlarGarLargst", "Passed - Expected FlarGarLargst");
     currentInputdisp = '0'
-=======
-          addDecimal();
-          addDigit('2');
-          addDecimal();
-          assert.equal(document.getElementById("screen").value, ".2", "Passed - Expected .2");
->>>>>>> origin/master
 });
