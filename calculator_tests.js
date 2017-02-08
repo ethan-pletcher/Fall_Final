95c1a8e782ab77a1f012744fff93e9c261890a43
Ethan S Pletcher <ep64523@srcs.loc>
2017-02-08 09:07:00 -0800

    fixed test ends

--- calculator_tests.js
+++ calculator_tests.js
@@ -3,18 +3,29 @@ QUnit.test("Add digits test", function (assert) {
     addDigit('1');
     addDigit('2');
     assert.equal(document.getElementById("screen").value, "12", "Passed - Expected 12");
-    allClear();
+    currentInputdisp = '';
+    currentInputreal = '';
 });
 // Test adding one, then two decimals
 QUnit.test("Add decimal test", function (assert) {
     addDecimal();
     addDigit('2');
     assert.equal(document.getElementById("screen").value, ".2", "Passed - Expected .2");
-    currentInputdisp = '0'
+    currentInputdisp = '';
+    currentInputreal = '';
 });
 QUnit.test("Test the displayErrorMessage function.", function (assert) {
     errormessage = "FlarGarLargst"
     displayErrorMessage()
     assert.equal(document.getElementById("screen2").value, "FlarGarLargst", "Passed - Expected FlarGarLargst");
-    currentInputdisp = '0'
+    currentInputdisp = '';
+    currentInputreal = '';
+});
+});
+QUnit.test("Test the lastDigitOperator function.", function (assert) {
+    errormessage = "FlarGarLargst"
+    displayErrorMessage()
+    assert.equal(document.getElementById("screen2").value, "FlarGarLargst", "Passed - Expected FlarGarLargst");
+    currentInputdisp = '';
+    currentInputreal = '';
 });
