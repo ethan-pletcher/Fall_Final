290a3185527609f715c6e1962ba0b442aed06949
MatthewGess <torresvilla79@gmail.com>
2017-02-08 08:51:18 -0800

    FlarGarLargst

--- calculator_tests.js
+++ calculator_tests.js
@@ -3,6 +3,7 @@ QUnit.test("Add digits test", function (assert) {
     addDigit('1');
     addDigit('2');
     assert.equal(document.getElementById("screen").value, "12", "Passed - Expected 12");
+    currentInputdisp = '0'
 });
 // Test adding one, then two decimals
 QUnit.test("Add decimal test", function (assert) {
@@ -10,4 +11,11 @@ QUnit.test("Add decimal test", function (assert) {
     addDigit('2');
     addDecimal();
     assert.equal(document.getElementById("screen").value, "0.2", "Passed - Expected 0.2");
+    currentInputdisp = '0'
+});
+QUnit.test("Test the displayErrorMessage function.", function (assert) {
+    errormessage = "FlarGarLargst"
+    displayErrorMessage()
+    assert.equal(document.getElementById("screen2").value, "FlarGarLargst", "Passed - Expected FlarGarLargst");
+    currentInputdisp = '0'
 });
